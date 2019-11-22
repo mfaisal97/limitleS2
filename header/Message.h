7fe242dee92496d81c81baa356e41bfe8ba9525f
Muhammad Faisal <mfaisal@aucegypt.edu>
2019-11-22 16:02:49 +0200

    Message class done .. not base64

--- header/Message.h
+++ header/Message.h
@@ -1,32 +1,39 @@
 #ifndef MESSAGE_H
 #define MESSAGE_H
 
+#include "../common/DataStructures.cpp"
 #include <string>
+
 
 #define size_t int
 
 enum MessageType { Request, Reply};
+enum OperationType {SignUp, SignIn, SignOut, UpdateInfo, GetOnline, SearchViewables, GetViewables};
 class Message
 {
     private:
         MessageType message_type;
-        int operation;
+        OperationType operation;
         void * message;
         size_t message_size;
         int rpc_id;
     public:
-        Message(int operation, void * p_message, size_t p_message_size,int p_rpc_id);
-        Message(char * marshalled_base64);
+        Message(OperationType _operation, void * p_message, size_t p_message_size, int p_rpc_id);
+        Message(char * _marshalled_base64);
+        Message(string _marshalled_base64);
+
         char * marshal ();
+        string marshalString ();
+
         int getOperation ();
         int getRPCId();
         void * getMessage();
         size_t getMessageSize();
         MessageType getMessageType();
-        void setOperation (int _operation);
-        void setMessage (void * message, size_t message_size);
-        void setMessageType (MessageType message_type);
-        char* habd;
+
+        void setOperation (OperationType _operation);
+        void setMessage (void * _message, size_t _message_size);
+        void setMessageType (MessageType _message_type);
         ~Message();
 };
 
--- source/Message.cpp
+++ source/Message.cpp
@@ -1,9 +1,72 @@
-Message::Message(int operation, void * p_message, size_t p_message_size,int p_rpc_id){
+Message::Message(OperationType _operation, void * p_message, size_t p_message_size, int p_rpc_id){
+  operation = _operation;
+  message = p_message;
+  message_size = p_message_size;
+  rpc = p_rpc_id;
+  message_type = MessageType::Request;
+}
 
+Message::Message(char * _marshalled_base64) : Message(FromCharArray(_marshalled_base64)){
 }
-Message::Message(char * marshalled_base64){
-    habd = marshalled_base64;
+
+Message::Message(string _marshalled_base64){
+  string str = _marshalled_base64;
+  message_type = MessageType(GetNumberBetweenBracket(str));
+  operation = OperationType(GetNumberBetweenBracket(str));
+  rpc_id = GetNumberBetweenBracket(str);
+  message_size = GetNumberBetweenBracket(str);
+  message = str;
 }
+
+char * Message::marshal (){
+  return ToCharArray(marshalString());
+}
+
+string Message::marshalString (){
+  string str = "";
+  str = str + NumberAsString(int(message_type));
+  str = str + NumberAsString(int(operation));
+  str = str + NumberAsString(int(rpc_id));
+  str = str + NumberAsString(int(message_size));
+  str = str + message;
+
+  return str;
+}
+
+
+OperationType Message::getOperation (){
+  return operation;
+}
+
+MessageType Message::getMessageType(){
+  return message_type;
+}
+
+int Message::getRPCId(){
+  return rpc_id;
+}
+
+void * Message::getMessage(){
+  return message;
+}
+
+size_t Message::getMessageSize(){
+  return message_size;
+}
+
+void Message::setOperation (OperationType _operation){
+  operation = _operation;
+}
+
+void Message::setMessage (void * _message, size_t _message_size){
+  message = _message;
+  message_size = _message_size;
+}
+
+void Message::setMessageType (MessageType message_type){
+  message_type = _message_type;
+}
+
 Message::~Message(){
 
 }
