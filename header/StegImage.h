22bb00bda833fff17406aaa5dd3d372b174cc773
Muhammad Faisal <mfaisal@aucegypt.edu>
2019-11-23 09:04:43 +0200

    StegImage hopefully complete (needs run test)

--- header/StegImage.h
+++ header/StegImage.h
@@ -1,29 +1,38 @@
 #ifndef StegImage_H
 #define StegImage_H
 
+#include "../common/DataStructures.cpp"
 
-class StegImage
-{
+class StegImage{
     private:
-
+        StegImageInfo info;
     public:
-        StegImage();
-        StegImage(string stegImagePath);      //open already StegImage
-        ~StegImage();
-        void AddImage(string fileName);
-        void AddUser(string userName, int views);
-        void Creator(string userName);
-
+        StegImage(string plainName, string creator);
+        StegImage(string plainName, string creator, map<string, int> remainingViews);
+        StegImage(StegImageInfo _info);
+        StegImage(string stegName);      //open already StegImage
 
         //add some modifiers
+        bool addImage(string plainName);
+        bool addUser(string userName, int views);
+        bool increaseViews(string userName, int inc);
+        bool setCreator(string userName);
 
+        //getters
+        bool canIncreaseViews(string name, int inc);
+        bool hasViews(string name);
+        int getUserViews(string name);
+        string getCreator();
 
-        //add some getters
+        string AsString();
+        char* AsCharArray();
 
+        // helpers
+        bool savePlainImage();
+        bool removePlainImage();
+        bool saveStegImage();
 
-        //
-
-        char* CreateStegImage();    //returns the default image with everything inside
+        ~StegImage();
 };
 
 #include "../source/StegImage.cpp"
---
+++ source/StegImage.cpp
@@ -0,0 +1,103 @@
+StegImage::StegImage(string plainName, string creator){
+    addImage(plainName);
+    setCreator(creator);
+}
+
+StegImage::StegImage(string plainName, string creator, map<string, int> remainingViews):StegImage(plainName, creator){
+  info.remainingViews = remainingViews;
+}
+
+StegImage::StegImage(StegImageInfo _info){
+  info.plainName = _info.plainName;
+  info.creator = _info.creator;
+  info.remainingViews = _info.remainingViews;
+  info.imageContent = _info.imageContent;
+}
+
+StegImage::StegImage(string stegName){
+  string InfoString = decode("", StegImagesDirectory + stegName, true);
+  info.Initialize(&InfoString);
+}
+
+bool StegImage::addImage(string plainName){
+  info.plainName = plainName;
+  info.imageContent = ReadImageBinaryAsString(PlainImagesDirectory + plainName);
+
+  return true;
+}
+
+bool StegImage::addUser(string userName, int views){
+  if(ValidUserNameString(userName)){
+    info.remainingViews[userName] = views;
+    return true;
+  }
+
+  return false;
+}
+
+bool StegImage::increaseViews(string userName, int inc){
+  if(canIncreaseViews(userName, inc)){
+    info.remainingViews[userName] = info.remainingViews[userName] + inc;
+    return true;
+  }
+
+  return false;
+}
+
+bool StegImage::setCreator(string userName){
+  if(ValidUserNameString(userName)){
+    info.creator = userName;
+    return true;
+  }
+  return false;
+}
+
+
+bool StegImage::canIncreaseViews(string name, int inc){
+  return (getUserViews(name) >= 0) &&  (getUserViews(name) + inc >= 0);
+}
+
+bool StegImage::hasViews(string name){
+  return getUserViews(name) > 0;
+}
+
+int StegImage::getUserViews(string name){
+  auto it = info.remainingViews.find(name);
+  if (it != info.remainingViews.end()){
+    if (info.remainingViews[name] >= 0){
+      return info.remainingViews[name];
+    }
+  }
+  return -1;
+}
+
+string StegImage::getCreator(string name){
+  return info.creator;
+}
+
+string StegImage::AsString(){
+  string hash = info.GetHash();
+  string encoding = Encode(info.AsString(), StegImagesDirectory + hash + ".jpeg");
+  return encoding;
+}
+
+char* StegImage::AsCharArray(){
+  return ToCharArray(AsString());
+}
+
+bool StegImage::savePlainImage(){
+  return WriteImageBinaryAsString(PlainImagesDirectory + info.plainName, info.imageContent);
+}
+
+bool StegImage::removePlainImage(){
+  return remove(PlainImagesDirectory + info.plainName) == 0;
+}
+
+bool StegImage::saveStegImage(){
+  string hash = info.GetHash();
+  string encoding = Encode(info.AsString(), StegImagesDirectory + hash + ".jpeg");
+  return true;
+}
+
+StegImage::~StegImage(){
+}
