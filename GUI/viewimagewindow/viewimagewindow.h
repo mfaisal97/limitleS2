7561670500833693c14e4304d20fde40b87829a7
ahmedkhatter <ahmedkhatter@aucegypt.edu>
2019-12-04 18:08:45 +0200

    View image integration in progress

--- GUI/viewimagewindow/viewimagewindow.h
+++ GUI/viewimagewindow/viewimagewindow.h
@@ -16,8 +16,8 @@ class ViewImageWindow : public QDialog
     Q_OBJECT
 
 public:
-    explicit ViewImageWindow(QWidget *parent = nullptr);
-    void showLists(std::map<std::string,std::string> *exMap=nullptr,std::map<std::string,std::string> *exMap2=nullptr);
+    explicit ViewImageWindow(QWidget *parent = nullptr,Peer* _peer=nullptr);
+    void showLists();
     void setPeer(Peer* _peer){
         peer = _peer;
     }
@@ -28,8 +28,14 @@ private slots:
     void on_pushButton_2_clicked();
     void on_pushButton_3_clicked();
 
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
 private:
     Peer* peer;
+    std::map<std::string, std::string> *localMap;
+    std::map<std::string, std::string> *remoteMap;
     Ui::ViewImageWindow *ui;
 };
 
